6825685a44c40e2bcd977afb53f5184f91b0891b
Ali Raghebi <aliraghebiii@gmail.com>
2024-09-25 14:01:54 +0330

    Update modals.js

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,23 +1,25 @@
 import {Modal} from 'bootstrap';
 
-let modalsElement = document.getElementById('laravel-livewire-modals');
+document.addEventListener('livewire:init', function () {
+    let modalsElement = document.getElementById('laravel-livewire-modals');
+    
+    modalsElement.addEventListener('hidden.bs.modal', () => {
+        Livewire.emit('resetModal');
+    });
+    
+    Livewire.on('showBootstrapModal', () => {
+        let modal = Modal.getInstance(modalsElement);
+    
+        if (!modal) {
+            modal = new Modal(modalsElement);
+        }
+    
+        modal.show();
+    });
 
-modalsElement.addEventListener('hidden.bs.modal', () => {
-    Livewire.emit('resetModal');
-});
+    Livewire.on('hideModal', () => {
+        let modal = Modal.getInstance(modalsElement);
 
-Livewire.on('showBootstrapModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    if (!modal) {
-        modal = new Modal(modalsElement);
-    }
-
-    modal.show();
-});
-
-Livewire.on('hideModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    modal.hide();
+        modal.hide();
+    });
 });
